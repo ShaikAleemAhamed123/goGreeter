28577b8e0483143b21a097990b584db2af7167e4
ShaikAleemAhamed123 <shaikaleemahamed786@gmail.com>
2024-09-09 00:28:20 +0530

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ShaikAleemAhamed123/goGreeter
+
+go 1.23.0
---
+++ main.go
@@ -0,0 +1,5 @@
+package goGreeter
+
+func GreetHello() string {
+	return "This is the greet from GreetHello() function in the goGreeter Package."
+}
